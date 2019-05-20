55471a278a32cc8635b045bd2544acf8cb2d4cdf
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-05-20 19:11:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const outerUL = document.querySelector('.tree');
+const li = outerUL.querySelectorAll('li');
+
+for (let i = 0; i < li.length; i++ ) {
+    const span = document.createElement('span');
+    li[i].insertBefore(span, li[i].firstChild);
+    span.append(span.nextSibling);
+}
+
+outerUL.addEventListener('click', change);
+function change (event) {
+const target = event.target;
+if (target.nodeName === 'SPAN') {
+    let childrenUL = target.parentNode.querySelector('ul');
+    childrenUL.hidden = !childrenUL.hidden;
+}
+}
+
+
